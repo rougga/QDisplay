032bd021d435da5c53c7b7ce7d1a3342ce350960
ROUGGA <rouggama@gmail.com>
2024-07-16 18:52:42 +0100

    v0.2

--- src/java/main/CfgHandler.java
+++ src/java/main/CfgHandler.java
@@ -22,7 +22,7 @@ public class CfgHandler {
     
     //MetaData
     public  static final String APP = "QDisplay";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String COMPANY = "ROUGGA";
     public static final String CLIENT = "NST-Maroc";
     public static final String YEAR = "2024";
