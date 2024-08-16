99165b4c44ce0f137e94ebc46c705aafe627703a
ROUGGA <bougado.youssef@hotmail.com>
2024-08-16 21:15:15 +0100

    QDisplay 0.3.2-LITE
    -changed "ZONE" button to be always displayed.

--- src/java/main/CfgHandler.java
+++ src/java/main/CfgHandler.java
@@ -22,7 +22,7 @@ public class CfgHandler {
     
     //MetaData
     public  static final String APP = "QDisplay";
-    public static final String VERSION = "0.3-LITE";
+    public static final String VERSION = "0.3.2-LITE";
     public static final String COMPANY = "ROUGGA";
     public static final String CLIENT = "NST-Maroc";
     public static final String YEAR = "2024";
